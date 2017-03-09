490a2a9b8d0e2be99722928e7d88cac9ddacefdf
volitilov <volitilov@gmail.com>
2017-03-09 22:35:43 +0300

    changes sidebar-menu

--- js/widjets/sidebar.js
+++ js/widjets/sidebar.js
@@ -5,19 +5,39 @@ $j(function() {
 	$j('#sidebar').click(function(event) {
 		event.preventDefault();
 		count ++;
-		
+
 		if(count % 2 == 0) {
 			$j('span').hide();
 			$j('.sidebar-menu').css({
 				width: '6%'
 			});
 		} else {
-			$j('span').show();
 			$j('.sidebar-menu').css({
-				width: '18%'
+				width: '21%'
 			});
+			$j('span').show();
 		}
 
 		return false;
 	});
+	$j('.sidebar-menu').mouseover(function(event) {
+		event.preventDefault();
+		$j(this).css({
+			width: '21%'
+		});
+		$j('span').show();
+
+		return false;
+	});
+
+	$j('.sidebar-menu').mouseout(function(event) {
+		event.preventDefault();
+		$j('span').hide();
+		$j(this).css({
+			width: '6%'
+		});
+
+		return false;
+	});
+
 });
