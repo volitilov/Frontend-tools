ce72eda087f6797292f3de0c0b1f4566254e0925
volitilov <volitilov@gmail.com>
2017-03-22 21:38:58 +0300

    finish widjet: list

--- js/widjets/list.js
+++ js/widjets/list.js
@@ -1,5 +1,22 @@
 var $j = jQuery.noConflict();
 
 $j(function() {
-	
+	$j('.search > input').keyup(function(event) {
+
+		var y = String($j(this).val());
+
+		$j('.list li').each(function(index) {
+			var x = $j(this).text();
+
+			if (x.indexOf(y) == -1) {
+				$j(this).hide();
+			}
+
+			if (y == '' || x.indexOf(y) != -1) {
+				$j(this).show();
+			}
+
+		});
+
+	});
 });
