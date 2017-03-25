ab422647fbe6fd09711161d88e4f76f07db1c20f
volitilov <volitilov@gmail.com>
2017-03-25 21:31:17 +0300

    finish widjet: dataview

--- js/widjets/dataview.js
+++ js/widjets/dataview.js
@@ -1,95 +1,134 @@
 var j = jQuery.noConflict();
 
 j(function() {
-	var count = true;
+    var count = true;
 
-	// get input value 
-	var input = '';
-	// get textarea value
-	var textarea = '';
+    j('#card-small').click(function(event) {
+        event = event || window.event;
+        event.preventDefault();
 
-	j('#card-small').click(function(event) {
-		event = event || window.event;
-		event.preventDefault();
+        if (count == false) {
+            j('.data-item').css({
+                width: '48%'
+            });
+        }
 
-		if (count == false) {
-			j('.data-item').css({
-				width: '48%'
-			});
-		}
+        count = true;
 
-		count = true;
+        return false;
+    });
 
-		return false;
-	});
+    j('#card-big').click(function(event) {
+        event = event || window.event;
+        event.preventDefault();
 
-	j('#card-big').click(function(event) {
-		event = event || window.event;
-		event.preventDefault();
+        if (count == true) {
+            j('.data-item').css({
+                width: '100%'
+            });
+        }
 
-		if (count == true) {
-			j('.data-item').css({
-				width: '100%'
-			});
-		}
+        count = false;
 
-		count = false;
-
-		return false;
-	});
+        return false;
+    });
 
 
-	// handler ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-	j('.add-content input, textarea').keyup(function(event) {
+    // remove item ::::::::::::::::::::::::::::::::::::::::::::::::::::
+    var list_select_item = [0,];
 
-		input = String(j('.add-content input').val());
-		textarea = String(j('.add-content textarea').val());
+    // add handler
+    j('.data-item').click(function(event) {
+        event = event || window.event;
+        event.preventDefault();
+
+        j(this).css({
+            borderColor: 'rgb(66, 133, 244)'
+        });
+
+        var id_item = list_select_item.length + 1;
+        var item = j(this).attr('id', id_item);
+        list_select_item.push(item);
+
+        return false;
+    });
+
+    list_select_item[0] = j('.data-item:first-child').clone(true);
+
+    j('.remove-content a').click(function(event) {
+        event = event || window.event;
+        event.preventDefault();
+
+        for (var i = 0; i < list_select_item.length; i++) {
+            if (list_select_item[i] != 0) {
+                j('.widjet-data').find(list_select_item[i]).remove();
+            }
+        }
+        list_select_item.length = 1;
+
+        return false;
+    });
+
+    // handler ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    // get input value
+    var input = '';
+    // get textarea value
+    var textarea = '';
+
+    j('.add-content input, textarea').keyup(function(event) {
+
+        input = String(j('.add-content input').val());
+        textarea = String(j('.add-content textarea').val());
 
 
-		if (input || textarea != '') {
+        if (input || textarea != '') {
 
-			j(this).css({
-				borderColor: ''
-			});
+            j(this).css({
+                borderColor: ''
+            });
 
-		}
-		
-	});
+        }
 
-	// add content ::::::::::::::::::::::::::::::::::::::::::::::::::::
-	j('.add-content button').click(function(event) {
-		event = event || window.event;
-		event.preventDefault();
+    });
 
-		// clone object
-		var item = j('.data-item:first-child').clone();
+    // add content ::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-		// find h4 in object
-		var name = item.find('h4');
-		// find p in object
-		var description = item.find('.content-description p');
+    j('.add-content button').click(function(event) {
+        event = event || window.event;
+        event.preventDefault();
+
+        // clone object
+        var item = list_select_item[0].clone(true);
+        // find h4 in object
+        var name = item.find('h4');
+        // find p in object
+        var description = item.find('.content-description p');
+
+        item.removeAttr('id');
+        item.css({
+            borderColor: ''
+        });
 
 
-		if (input == '') {
-			j('.add-content input').css({
-				borderColor: 'red'
-			});
-		}
+        if (input == '') {
+            j('.add-content input').css({
+                borderColor: 'red'
+            });
+        }
 
-		if (textarea == '') {
-			j('.add-content textarea').css({
-				borderColor: 'red'
-			});
-		}
+        if (textarea == '') {
+            j('.add-content textarea').css({
+                borderColor: 'red'
+            });
+        }
 
-		if (input && textarea != '') {
-			name.text(input);
-			description.text(textarea);
-			j('.widjet-data').append(item);
-		}
+        if (input && textarea != '') {
+            name.text(input);
+            description.text(textarea);
+            j('.widjet-data').append(item);
+        }
 
-		return false;
-	});
-
+        return false;
+    });
 
 });
